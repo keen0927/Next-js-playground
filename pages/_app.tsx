575c162dc1cd6f52108a5725f26c2fb079859fd8
KEEN <kyh0927@live.co.kr>
2021-03-02 01:10:16 +0900

    feat : global app test

---
+++ pages/_app.tsx
@@ -0,0 +1,19 @@
+import Header from "./header";
+
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+      <style jsx global>
+        {`
+          body {
+            margin: 0;
+          }
+        `}
+      </style>
+    </>
+  );
+};
+
+export default MyApp;
